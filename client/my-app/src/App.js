2450e37e4bcd514c93cf597d739b72a24f3a113a
yutank34 <yuta34@gmail.com>
2021-07-05 07:53:34 +0900

    refactor: modify set up apollo client

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -2,10 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import './index.js'
 
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+var cnt = 0
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  console.log(cnt++);
+  console.log(loading);
+  console.log(error);
+  console.log(data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
 function App() {
   return (
     <div>
       <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates />
     </div>
   );
 }
--- client/my-app/src/index.js
+++ client/my-app/src/index.js
@@ -16,52 +16,9 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
-client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
-
-
-const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
-  }
-`;
-
-var cnt = 0
-function ExchangeRates() {
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
-  console.log(cnt++);
-  console.log(loading);
-  console.log(error);
-  console.log(data);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data.rates.map(({ currency, rate }) => (
-    <div key={currency}>
-      <p>
-        {currency}: {rate}
-      </p>
-    </div>
-  ));
-}
-
 render(
   <ApolloProvider client={client}>
     <App />
-    <ExchangeRates />
     </ApolloProvider>,
   document.getElementById('root')
 );
